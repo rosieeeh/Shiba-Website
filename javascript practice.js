b9bd72a592ee106f7f1d8702529e78c0410df3a3
rosieeeh <40476532+rosieeeh@users.noreply.github.com>
2018-07-17 15:52:09 -0700

    First Commit

---
+++ javascript practice.js
@@ -0,0 +1,11 @@
+if button {}
+  onclick="alert('Hellooo')">Click meee</button>
+alert=("Hey! This is an alert box, are you ready for the website");
+
+// var my Button= document.querySelector('button');
+// var myHeading= document.querySelector("h1");
+//
+// myButton.onclick= function(){
+//   var name = pormpt('Please enter your name');
+//   localStorage.setItem("name: ",name);
+// }
